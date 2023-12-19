c6561ed12d2fb8d9745e9ce131089def0e764b9a
conrad.anderson75926 <conrad.anderson75926@gmail.com>
2023-12-19 17:34:38 +0900

    pre-processing finished

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+from tensorflow import keras
 
+###################################################
 fname = os.path.join("jena_climate_2009_2016.csv")
   
 with open(fname) as f:
@@ -16,8 +18,11 @@ raw_data = np.zeros((len(lines), len(header) - 1))
 for i, line in enumerate(lines):
     values = [float(x) for x in line.split(",")[1:]]
     temperature[i] = values[1]                       
-    raw_data[i, :] = values[:]     
+    raw_data[i, :] = values[:]  
+#####################################################
+   
 
+#####################################################
 num_train_samples = int(0.5 * len(raw_data))
 num_val_samples = int(0.25 * len(raw_data))
 num_test_samples = len(raw_data) - num_train_samples - num_val_samples
@@ -27,4 +32,48 @@ raw_data -= mean
 std = raw_data[:num_train_samples].std(axis=0)
 raw_data /= std # Result : -1 ~ +1
 
-print(raw_data.mean(axis=0))
+mean_tem = temperature[:num_train_samples].mean(axis=0)
+temperature -= mean_tem
+std_mean = temperature[:num_train_samples].std(axis=0)
+temperature /= std_mean # Result : -1 ~ +1
+#####################################################
+
+
+######################################################
+sampling_rate = 6 
+sequence_length = 120 
+delay = sampling_rate * (sequence_length + 24 - 1)
+batch_size = 256 
+  
+train_dataset = keras.utils.timeseries_dataset_from_array(
+    data = raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=0,
+    end_index=num_train_samples)
+  
+val_dataset = keras.utils.timeseries_dataset_from_array(
+    data = raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=num_train_samples,
+    end_index=num_train_samples + num_val_samples)
+  
+test_dataset = keras.utils.timeseries_dataset_from_array(
+    data = raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=num_train_samples + num_val_samples)
+#############################################################
+
+for inputs, targets in test_dataset:
+    print(inputs, " : ", targets, "\n")
